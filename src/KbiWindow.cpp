43e018183d8e70dd3bf9b8c02d46559877752adf
Mloser <975525512@qq.com>
2021-04-18 14:00:58 +0800

    Make some change in KbiWindow .h/.cpp like comment in "KbiWindow.h" says. Increase three boxes and a button, but kbi_box1 is empty.

--- src/KbiWindow.cpp
+++ src/KbiWindow.cpp
@@ -11,29 +11,62 @@
 #define SCIENTIFIC_NOTATION
 #undef  SCIENTIFIC_NOTATION
 
+
 KbiWindow::KbiWindow(Player* p_player)
-        :m_button("Play"), player(p_player)
+        :
+        kbi_box_top(Gtk::Orientation::VERTICAL, 0),
+        kbi_box1(Gtk::Orientation::VERTICAL, 20),
+        kbi_box2(Gtk::Orientation::HORIZONTAL, 20),
+        kbi_button_control_play_or_stop("Play"),
+        kbi_button_quit("Close"),
+        player(p_player)
 {
-    m_button.set_margin(100);
-    m_button.signal_clicked().connect(sigc::mem_fun(*this, &KbiWindow::on_button_clicked));
+
+    //set child widgets
+    set_child(kbi_box_top);
+    set_title("kbi");
+    set_default_size(KbiWindow::kbi_window_width, KbiWindow::kbi_window_height);
+    kbi_box_top.append(kbi_box1);
+    kbi_box_top.append(kbi_box2);
+    kbi_box1.set_size_request(KbiWindow::kbi_window_width, KbiWindow::kbi_window_height - 100);
+    kbi_box1.set_expand(true);
+    kbi_box2.append(kbi_button_control_play_or_stop);
+    kbi_box2.append(kbi_button_quit);
+
+    //set kbi_button_control_play_or_stop
+    kbi_button_control_play_or_stop.set_margin(10);
+    kbi_button_control_play_or_stop.set_size_request(KbiWindow::kbi_window_width / 2);
+    kbi_button_control_play_or_stop.set_expand(true);
+    kbi_button_control_play_or_stop.signal_clicked().connect(sigc::mem_fun(*this, &KbiWindow::on_button_control_play_or_stop_clicked));
+
+    //set kbi_button_quit
+    kbi_button_quit.set_margin(10);
+    kbi_button_quit.set_size_request(KbiWindow::kbi_window_width / 2);
+    kbi_button_quit.set_expand(true);
+    kbi_button_quit.signal_clicked().connect(sigc::mem_fun(*this, &KbiWindow::on_button_quit_clicked));
+
     event_controller_key = Gtk::EventControllerKey::create();
     event_controller_key->signal_key_pressed().connect(sigc::mem_fun(*this, &KbiWindow::on_key_pressed), false);
     event_controller_key->signal_key_released().connect(sigc::mem_fun(*this, &KbiWindow::on_key_released), true);
-    set_child(m_button);
+
     setup();
 }
 
-void KbiWindow::on_button_clicked()
+void KbiWindow::on_button_control_play_or_stop_clicked()
 {
     bool isPlaying = player->toggle();
     if (isPlaying) {
-        m_button.set_label("Stop");
+        kbi_button_control_play_or_stop.set_label("Stop");
         add_controller(event_controller_key);
     }
     else {
-        m_button.set_label("Play");
+        kbi_button_control_play_or_stop.set_label("Play");
         remove_controller(event_controller_key);
     }
+}
+
+void KbiWindow::on_button_quit_clicked() {
+    hide();
 }
 
 KbiWindow::~KbiWindow()
--- src/include/KbiWindow.h
+++ src/include/KbiWindow.h
@@ -1,5 +1,18 @@
 //
 // Created by mike on 4/7/21.
+
+/* changed by zhang on 4/18/21:
+ * m_button is changed. (kbi_button_control_play_or_stop)
+ * on_button_clicked() is changed. (on_button_control_play_or_stop())
+ * set some attributes of the window.
+ * increase private: kbi_button_quit, kbi_box_top, kbi_box1, kbi_box2.
+ * kbi_box1 and kbi_box2 is in kbi_box_top.
+ * kbi_box1 is empty. It is planned to place the unit that prints notes.
+ * kbi_button_control_play_or_stop and kbi_button_quit is in kbi_box2.
+ * increase protected: on_button_quit_clicked().
+ *increase private: kbi_window_width(int), kbi_window_height(int)
+ *
+ */
 //
 
 #ifndef KBI_KBIWINDOW_H
@@ -9,6 +22,7 @@
 #include <gtkmm/button.h>
 #include <gtkmm/eventcontroller.h>
 #include <gtkmm/eventcontrollerkey.h>
+#include <gtkmm/box.h>
 #include "portaudio.h"
 #include "Player.h"
 
@@ -21,14 +35,21 @@ public:
     ~KbiWindow() override;
 
 protected:
-    void on_button_clicked();
+    void on_button_control_play_or_stop_clicked();
 
     bool on_key_pressed(guint keyVal, guint, Gdk::ModifierType state);
 
     void on_key_released(guint keyVal, guint, Gdk::ModifierType state);
 
+    void on_button_quit_clicked();
+
 private:
-    Gtk::Button m_button;
+
+    int kbi_window_width = 300;            //decide window's width
+    int kbi_window_height = 400;           //decide window's height
+    Gtk::Box kbi_box_top, kbi_box1, kbi_box2;
+    Gtk::Button kbi_button_control_play_or_stop, kbi_button_quit;
+
     Player* player;
     Glib::RefPtr<Gtk::EventControllerKey> event_controller_key;
 

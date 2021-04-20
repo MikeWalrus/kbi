d6279f7eaeaecb92e1c479d36ec3f074279399ea
Zhang-Hao0613 <zh975525512@163.com>
2021-04-20 11:44:37 +0800

    increase some attributes of buttons to change their shape when becoming big.

--- src/KbiWindow.cpp
+++ src/KbiWindow.cpp
@@ -34,16 +34,21 @@ KbiWindow::KbiWindow(Player* p_player)
 
     //set kbi_button_control_play_or_stop
     kbi_button_control_play_or_stop.set_margin(10);
-    kbi_button_control_play_or_stop.set_size_request(KbiWindow::kbi_window_width/2);
-    kbi_button_control_play_or_stop.set_expand(true);
+    kbi_button_control_play_or_stop.set_vexpand(false);
+    kbi_button_control_play_or_stop.set_hexpand(true);
+    kbi_button_control_play_or_stop.set_valign(Gtk::Align::CENTER);
+    kbi_button_control_play_or_stop.set_halign(Gtk::Align::FILL);
     kbi_button_control_play_or_stop.signal_clicked().connect(
             sigc::mem_fun(*this, &KbiWindow::on_button_control_play_or_stop_clicked));
 
     //set kbi_button_quit
     kbi_button_quit.set_margin(10);
-    kbi_button_quit.set_size_request(KbiWindow::kbi_window_width/2);
-    kbi_button_quit.set_expand(true);
-    kbi_button_quit.signal_clicked().connect(sigc::mem_fun(*this, &KbiWindow::on_button_quit_clicked));
+    kbi_button_quit.set_vexpand(false);
+    kbi_button_quit.set_hexpand(true);
+    kbi_button_quit.set_valign(Gtk::Align::CENTER);
+    kbi_button_quit.set_halign(Gtk::Align::FILL);
+    kbi_button_quit.signal_clicked().connect(
+            sigc::mem_fun(*this, &KbiWindow::on_button_quit_clicked));
 
     event_controller_key = Gtk::EventControllerKey::create();
     event_controller_key->signal_key_pressed().connect(sigc::mem_fun(*this, &KbiWindow::on_key_pressed), false);

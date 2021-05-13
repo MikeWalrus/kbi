59c9fae7be172750678a194db307205a4a24c56b
Zhang Hao <zh975525512@163.com>
2021-05-13 17:26:56 +0800

    add combobox and treemodelcolumn, but have not shown in program.

--- src/include/KbiWindow.h
+++ src/include/KbiWindow.h
@@ -11,8 +11,11 @@
 #include <gtkmm/eventcontroller.h>
 #include <gtkmm/eventcontrollerkey.h>
 #include <gtkmm/box.h>
+#include <gtkmm/treemodel.h>
+#include <gtkmm/combobox.h>
 #include <functional>
 #include <map>
+#include <gtkmm/liststore.h>
 #include "portaudio.h"
 #include "Ctrl.h"
 #include "Player.h"
@@ -34,11 +37,13 @@ protected:
 
     void on_switch_control_voices_limit_clicked();
 
+    void on_combobox();
+
 private:
 
     int kbi_window_width = 600;            //decide window's width
     int kbi_window_height = 700;           //decide window's height
-    Gtk::Box kbi_box_top, kbi_box1, kbi_box2, kbi_box_switch;
+    Gtk::Box kbi_box_top, kbi_box1, kbi_box2, kbi_box_switch, kbi_box_combobox;
     Gtk::Button kbi_button_control_play_or_stop, kbi_button_quit;
     Gtk::Switch kbi_switch_control_voices_limit;
 
@@ -47,7 +52,21 @@ private:
     typedef function<shared_ptr<Ctrl>(Player*)> CtrlFactoryMethod;
     const std::map<string, CtrlFactoryMethod> controllers = {{"default", Controller::create}};
     shared_ptr<Ctrl> controller;
+
+    class ModelColumns : public Gtk::TreeModel::ColumnRecord {
+    public:
+        ModelColumns(){
+            add(kbi_instruments_name);
+        }
+        Gtk::TreeModelColumn<Glib::ustring> kbi_instruments_name;
+    };
+
+    ModelColumns kbi_columns;
+    Gtk::ComboBox kbi_combobox_instruments;
+    Glib::RefPtr<Gtk::ListStore> kbi_ref_treemodel;
 };
+
+
 
 void setup();//use this to do any initialisation if you want.
 
